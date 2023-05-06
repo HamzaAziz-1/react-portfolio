eb5e3ba35a7d95141ff53ea1f6667e7ceff509e1
HamzaAziz-1 <hamza.aziz8750@gmail.com>
2023-05-06 18:55:12 +0500

    edit logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -37,7 +37,11 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            <img src={logo} alt="Logo" />
+            <h3 style={{color:"white"}}>
+              <strong>
+              HAMZA
+              </strong>  
+            </h3>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
