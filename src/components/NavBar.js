811381b4381fdb9526a7d240a83b1f58da034f0d
HamzaAziz-1 <hamza.aziz8750@gmail.com>
2023-11-01 18:33:02 +0500

    add links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
+/* eslint-disable react/jsx-no-target-blank */
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
+import navIcon3 from '../assets/img/download.svg';
+import navIcon4 from '../assets/img/document.svg';
 import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
@@ -78,17 +79,23 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
+                <a href="https://github.com/HamzaAziz-1" target="_blank">
+                  <img src={navIcon3} alt="" />
+                </a>
                 <a
                   href="https://www.linkedin.com/in/muhammad-hamza-aziz"
                   target="_blank"
                 >
                   <img src={navIcon1} alt="" />
                 </a>
+                <a
+                  href="https://drive.google.com/file/d/1q1-eCrTUhR2TqSc02FYTDAJpFbnEk2a-/view?usp=sharing"
+                  target="_blank"
+                >
+                  <img src={navIcon4} alt="" />
+                </a>
                 <a href="https://www.fiverr.com/hamzaaaziz" target="_blank">
                   <img src={navIcon2} alt="" />
-                </a>
-                <a href="#">
-                  <img src={navIcon3} alt="" />
                 </a>
               </div>
               <HashLink to="#connect">
